4ab88499db8df36056899dc31f1460bf75f6a00f
zhaoxiaogang <zhaoxiaogang@longtugame.com>
2014-06-22 19:01:03 +0800

    update new api;

---
+++ src/fast/rocket/sample/FundObj.java
@@ -0,0 +1,375 @@
+package fast.rocket.sample;
+
+public class FundObj {
+	private String icon;
+	private String jzrq;
+	private String jjdm;
+	private String hb1n;
+
+	private String qrsy;
+
+	private String jjjz;
+	private String wfsy;
+
+	private String jjmc;
+
+	private String jjfl;
+	private String startBuy;
+	private String zfxz;
+
+	private String hb1y;
+	private String hb3y;
+	private String hb6y;
+	private String hbjn;
+	private String sgbz;
+	private String shbz;
+
+	private String ssgs;
+	private String hygrsb3y;
+	private String jjgs;
+	private String fd;
+	private String jjgm;
+	private String jgyh;
+
+	private String jjzq;
+	private String multi;
+	private String tip;
+	private String fee;
+	private String zsgje;
+
+	private String zgmbs;
+	private String rjje;
+	private String startMsg;
+	private String feeMsg;
+	private String fundCompanyLogoUrl;
+	private String fundType;
+
+	private String fundTrend;
+	private String fundIntroduce;
+	private String fundSecurity;
+	private String detailImageUrl;
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getJzrq() {
+		return jzrq;
+	}
+
+	public void setJzrq(String jzrq) {
+		this.jzrq = jzrq;
+	}
+
+	public String getJjdm() {
+		return jjdm;
+	}
+
+	public void setJjdm(String jjdm) {
+		this.jjdm = jjdm;
+	}
+
+	public String getHb1n() {
+		return hb1n;
+	}
+
+	public void setHb1n(String hb1n) {
+		this.hb1n = hb1n;
+	}
+
+	public String getQrsy() {
+		return qrsy;
+	}
+
+	public void setQrsy(String qrsy) {
+		this.qrsy = qrsy;
+	}
+
+	public String getJjjz() {
+		return jjjz;
+	}
+
+	public void setJjjz(String jjjz) {
+		this.jjjz = jjjz;
+	}
+
+	public String getWfsy() {
+		return wfsy;
+	}
+
+	public void setWfsy(String wfsy) {
+		this.wfsy = wfsy;
+	}
+
+	public String getJjmc() {
+		return jjmc;
+	}
+
+	public void setJjmc(String jjmc) {
+		this.jjmc = jjmc;
+	}
+
+	public String getJjfl() {
+		return jjfl;
+	}
+
+	public void setJjfl(String jjfl) {
+		this.jjfl = jjfl;
+	}
+
+	public String getStartBuy() {
+		return startBuy;
+	}
+
+	public void setStartBuy(String startBuy) {
+		this.startBuy = startBuy;
+	}
+
+	public String getZfxz() {
+		return zfxz;
+	}
+
+	public void setZfxz(String zfxz) {
+		this.zfxz = zfxz;
+	}
+
+	public String getHb1y() {
+		return hb1y;
+	}
+
+	public void setHb1y(String hb1y) {
+		this.hb1y = hb1y;
+	}
+
+	public String getHb3y() {
+		return hb3y;
+	}
+
+	public void setHb3y(String hb3y) {
+		this.hb3y = hb3y;
+	}
+
+	public String getHb6y() {
+		return hb6y;
+	}
+
+	public void setHb6y(String hb6y) {
+		this.hb6y = hb6y;
+	}
+
+	public String getHbjn() {
+		return hbjn;
+	}
+
+	public void setHbjn(String hbjn) {
+		this.hbjn = hbjn;
+	}
+
+	public String getSgbz() {
+		return sgbz;
+	}
+
+	public void setSgbz(String sgbz) {
+		this.sgbz = sgbz;
+	}
+
+	public String getShbz() {
+		return shbz;
+	}
+
+	public void setShbz(String shbz) {
+		this.shbz = shbz;
+	}
+
+	public String getSsgs() {
+		return ssgs;
+	}
+
+	public void setSsgs(String ssgs) {
+		this.ssgs = ssgs;
+	}
+
+	public String getHygrsb3y() {
+		return hygrsb3y;
+	}
+
+	public void setHygrsb3y(String hygrsb3y) {
+		this.hygrsb3y = hygrsb3y;
+	}
+
+	public String getJjgs() {
+		return jjgs;
+	}
+
+	public void setJjgs(String jjgs) {
+		this.jjgs = jjgs;
+	}
+
+	public String getFd() {
+		return fd;
+	}
+
+	public void setFd(String fd) {
+		this.fd = fd;
+	}
+
+	public String getJjgm() {
+		return jjgm;
+	}
+
+	public void setJjgm(String jjgm) {
+		this.jjgm = jjgm;
+	}
+
+	public String getJgyh() {
+		return jgyh;
+	}
+
+	public void setJgyh(String jgyh) {
+		this.jgyh = jgyh;
+	}
+
+	public String getJjzq() {
+		return jjzq;
+	}
+
+	public void setJjzq(String jjzq) {
+		this.jjzq = jjzq;
+	}
+
+	public String getMulti() {
+		return multi;
+	}
+
+	public void setMulti(String multi) {
+		this.multi = multi;
+	}
+
+	public String getTip() {
+		return tip;
+	}
+
+	public void setTip(String tip) {
+		this.tip = tip;
+	}
+
+	public String getFee() {
+		return fee;
+	}
+
+	public void setFee(String fee) {
+		this.fee = fee;
+	}
+
+	public String getZsgje() {
+		return zsgje;
+	}
+
+	public void setZsgje(String zsgje) {
+		this.zsgje = zsgje;
+	}
+
+	public String getZgmbs() {
+		return zgmbs;
+	}
+
+	public void setZgmbs(String zgmbs) {
+		this.zgmbs = zgmbs;
+	}
+
+	public String getRjje() {
+		return rjje;
+	}
+
+	public void setRjje(String rjje) {
+		this.rjje = rjje;
+	}
+
+	public String getStartMsg() {
+		return startMsg;
+	}
+
+	public void setStartMsg(String startMsg) {
+		this.startMsg = startMsg;
+	}
+
+	public String getFeeMsg() {
+		return feeMsg;
+	}
+
+	public void setFeeMsg(String feeMsg) {
+		this.feeMsg = feeMsg;
+	}
+
+	public String getFundCompanyLogoUrl() {
+		return fundCompanyLogoUrl;
+	}
+
+	public void setFundCompanyLogoUrl(String fundCompanyLogoUrl) {
+		this.fundCompanyLogoUrl = fundCompanyLogoUrl;
+	}
+
+	public String getFundType() {
+		return fundType;
+	}
+
+	public void setFundType(String fundType) {
+		this.fundType = fundType;
+	}
+
+	public String getFundTrend() {
+		return fundTrend;
+	}
+
+	public void setFundTrend(String fundTrend) {
+		this.fundTrend = fundTrend;
+	}
+
+	public String getFundIntroduce() {
+		return fundIntroduce;
+	}
+
+	public void setFundIntroduce(String fundIntroduce) {
+		this.fundIntroduce = fundIntroduce;
+	}
+
+	public String getFundSecurity() {
+		return fundSecurity;
+	}
+
+	public void setFundSecurity(String fundSecurity) {
+		this.fundSecurity = fundSecurity;
+	}
+
+	public String getDetailImageUrl() {
+		return detailImageUrl;
+	}
+
+	public void setDetailImageUrl(String detailImageUrl) {
+		this.detailImageUrl = detailImageUrl;
+	}
+
+	@Override
+	public String toString() {
+		return "FundInfo [icon=" + icon + ", jzrq=" + jzrq + ", jjdm=" + jjdm
+				+ ", hb1n=" + hb1n + ", qrsy=" + qrsy + ", jjjz=" + jjjz
+				+ ", wfsy=" + wfsy + ", jjmc=" + jjmc + ", jjfl=" + jjfl
+				+ ", startBuy=" + startBuy + ", zfxz=" + zfxz + ", hb1y="
+				+ hb1y + ", hb3y=" + hb3y + ", hb6y=" + hb6y + ", hbjn=" + hbjn
+				+ ", sgbz=" + sgbz + ", shbz=" + shbz + ", ssgs=" + ssgs
+				+ ", hygrsb3y=" + hygrsb3y + ", jjgs=" + jjgs + ", fd=" + fd
+				+ ", jjgm=" + jjgm + ", jgyh=" + jgyh + ", jjzq=" + jjzq
+				+ ", multi=" + multi + ", tip=" + tip + ", fee=" + fee
+				+ ", zsgje=" + zsgje + ", zgmbs=" + zgmbs + ", rjje=" + rjje
+				+ ", startMsg=" + startMsg + ", feeMsg=" + feeMsg
+				+ ", fundCompanyLogoUrl=" + fundCompanyLogoUrl + ", fundType="
+				+ fundType + ", fundTrend=" + fundTrend + ", fundIntroduce="
+				+ fundIntroduce + ", fundSecurity=" + fundSecurity
+				+ ", detailImageUrl=" + detailImageUrl + "]";
+	}
+
+}
